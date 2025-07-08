53a271d5888b0dc3742316e48ba129b6f495579b
Igor Semyonov <igor@semyonov.xyz>
2025-07-07 23:38:15 -0400

    roulette is spinning!

--- examples/roulette.rs
+++ examples/roulette.rs
@@ -16,17 +16,51 @@ fn main() -> ! {
     let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
-    let light_it_all = [
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-    ];
+    // let light_it_all = [
+    //     [1, 1, 1, 1, 1],
+    //     [1, 1, 1, 1, 1],
+    //     [1, 1, 1, 1, 1],
+    //     [1, 1, 1, 1, 1],
+    //     [1, 1, 1, 1, 1],
+    // ];
 
-        loop {
-            display.show(&mut timer, light_it_all, 1000);
-            display.clear();
-            timer.delay_ms(1000u32);
+    // let mut light_pin_1 = [[0; 5]; 5];
+    // light_pin_1[0][0] = 1;
+    let mut lights = [[[0; 5]; 5]; 16];
+    for k in 0..5 {
+        lights[k][0][k] = 1;
+    }
+    for k in 5..9 {
+        lights[k][k - 4][4] = 1;
+    }
+    for k in 9..13 {
+        lights[k][4][12 - k] = 1;
+    }
+    for k in 13..16 {
+        lights[k][16-k][0] = 1;
+    }
+
+    loop {
+        // display.show(
+        //     &mut timer,
+        //     light_it_all,
+        //     1000,
+        // );
+        // display.clear();
+        // timer.delay_ms(1000u32);
+        for k in 0..16{
+            display.show(
+                &mut timer,
+                lights[k],
+                40u32, // at 20 the mux causes weird output
+            )
         }
+
+        // display.clear();
+        // display.show(
+        //     &mut timer,
+        //     light_pin_1,
+        //     2500,
+        // )
+    }
 }
