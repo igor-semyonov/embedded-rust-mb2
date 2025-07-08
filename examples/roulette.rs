c5a79832b20628ea5adf55e0b13713ce1d1d4ea7
Igor Semyonov <igor@semyonov.xyz>
2025-07-07 22:50:35 -0400

    roulette initially working

---
+++ examples/roulette.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use embedded_hal::delay::DelayNs;
+use microbit::{
+    board::Board, display::blocking::Display, hal::Timer,
+};
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let light_it_all = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+    ];
+
+        loop {
+            display.show(&mut timer, light_it_all, 1000);
+            display.clear();
+            timer.delay_ms(1000u32);
+        }
+}
